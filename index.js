df7745d2cec43e26a0237fd800043998a55bdf09
Erick Vargas <erickkvargas@gmail.com>
2022-04-05 16:07:30 -0700

    commit tree a56ed64a49b7f5190e0154cccf885dab2a52cff2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+function writeCards(array, eventName) {
+    let messageArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let name = array[i];
+        let message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+        messageArray.push(message);
+    }
+    return messageArray;
+}
+
+function countDownn(number){
+    while (number >= 0){
+console.log(number-=1)
+
+    }
+return number
+}
+countDownn(11)
+
+
+function countDown(numbers){
+    while (numbers>=0){
+        console.log(numbers);
+        numbers--;
+    }
+}
+countDown(4);
